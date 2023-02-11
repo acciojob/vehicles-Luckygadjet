fe30d616d29c26edc78817b39f3ba9c27d46c7a2
Your Name <sabavathnaveen951@gmail.com>
2023-02-11 23:31:48 +0530

    Second Last attempt

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,22 +2,15 @@ package com.driver;
 
 public class Boat implements WaterVehicle{
     private String name;
-
-    public Boat(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
-
     private int capacity;
-
-
-    @Override
-    public String getVehicleName() {
-        return null;
-    }
 
     @Override
     public int getVehicleCapacity() {
-        return 0;
+        return this.capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return this.name;
     }
 }
