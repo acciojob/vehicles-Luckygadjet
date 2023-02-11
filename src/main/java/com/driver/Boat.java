0b1618f98b22ea3d12ed441250a5a0ec9f8106c3
Your Name <sabavathnaveen951@gmail.com>
2023-02-11 23:17:53 +0530

    9th Attempt of Backend Assighnment

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,26 +4,6 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCapacity() {
-        return this.capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public Boat(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
 
     @Override
     public String getVehicleName() {
